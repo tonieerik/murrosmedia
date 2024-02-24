87b6fe2664d595e1af450744e98aef0cbaeb2cbb
Toni Leppänen <toni.leppanen@gmail.com>
2024-02-24 19:38:45 +0200

    Default values for sanity client

--- utils/sanity.ts
+++ utils/sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "q265orxf";
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "mandariinimedia";
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-01-27";
 
 export const client = createClient({
