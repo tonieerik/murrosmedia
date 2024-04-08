789cd74e9f83617fdb53aebf53d20c11e9cc497e
Toni Leppänen <toni.leppanen@gmail.com>
2024-04-08 10:44:23 +0300

    Optimise images

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ const headerText = <>Luova kumppanisi vastuullisuusviestintään</>;
 const Header = () => (
   <div className="text-gray-100">
     <div className="lg:hidden">
-      <div className="bg-cover bg-[url('/freelance-sisallontuottaja-mobile.jpg')]">
+      <div className="bg-cover bg-[url('/freelance-sisallontuottaja-mobile.webp')]">
         <div className="flex flex-col justify-end h-64 mobileHeaderImage">
           <div className="flex flex-row justify-end w-fit p-2 mx-2">
             <div className="w-full flex-col items-end bg-teal rounded">
@@ -19,7 +19,7 @@ const Header = () => (
     </div>
     <div className="hidden lg:max-xl:block">
       <div className="bg-teal flex">
-        <div className="w-2/5 h-[380px] bg-cover bg-[url('/freelance-sisallontuottaja-tablet.png')]"></div>
+        <div className="w-2/5 h-[380px] bg-cover bg-[url('/freelance-sisallontuottaja-tablet.webp')]"></div>
         <div className="flex flex-col justify-end w-3/5 pb-12">
           <div className="text-6xl font-bold handwriting">{nameText}</div>
           <br />
@@ -31,7 +31,7 @@ const Header = () => (
     </div>
     <div className="hidden xl:max-xxl:block">
       <div className="bg-teal flex">
-        <div className="w-1/2 h-[530px] bg-cover bg-[url('/freelance-sisallontuottaja-full.png')]"></div>
+        <div className="w-1/2 h-[530px] bg-cover bg-[url('/freelance-sisallontuottaja-full.webp')]"></div>
         <div className="flex flex-col justify-end w-1/2 pb-12">
           <div className="text-8xl handwriting">{nameText}</div>
           <br />
@@ -43,7 +43,7 @@ const Header = () => (
     </div>
     <div className="hidden xxl:block">
       <div className="bg-teal flex">
-        <div className="w-1/2 h-[640px] bg-cover bg-[url('/freelance-sisallontuottaja-full.png')]"></div>
+        <div className="w-1/2 h-[640px] bg-cover bg-[url('/freelance-sisallontuottaja-full.webp')]"></div>
         <div className="flex flex-col justify-end w-1/2 pb-12">
           <div className="text-4xl font-bold handwriting">{nameText}</div>
           <br />
