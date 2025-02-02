921bf175df191840082877d5a9cbfe594ac7a033
Toni Leppänen <toni.leppanen@gmail.com>
2025-02-02 22:56:41 +0000

    Optimise to use next/font

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+
 config.autoAddCss = false;
 
 const titillium = Titillium_Web({
@@ -17,14 +18,6 @@ const RootLayout = ({
   children: React.ReactNode;
 }>) => (
   <html lang="en">
-    <head>
-      <link
-        rel="preconnect"
-        href="https://fonts.gstatic.com"
-        crossOrigin="anonymous"
-      />
-      <link rel="dns-prefetch" href="https://fonts.gstatic.com" />
-    </head>
     <body className={titillium.className}>{children}</body>
     <GoogleAnalytics gaId="G-MCDZBZK9JH" />
   </html>
--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,13 @@
+import { Caveat } from "next/font/google";
+
 const nameText = "Nelli Leppänen";
 const headerText = <>Kuunteleva kumppanisi vastuullisuusviestintään</>;
+
+const caveat = Caveat({
+  weight: ["700"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 const Header = () => (
   <div className="text-gray-100">
@@ -9,7 +17,9 @@ const Header = () => (
           <div className="flex flex-row justify-end w-fit p-2 mx-2">
             <div className="w-full flex-col items-end bg-teal rounded">
               <div className="px-2 pt-2 pb-1 leading-none">
-                <strong className="handwriting text-3xl">{nameText}</strong>
+                <strong className={`${caveat.className} text-3xl`}>
+                  {nameText}
+                </strong>
               </div>
               <div className="px-2 pb-2 text-sm leading-snug">{headerText}</div>
             </div>
@@ -21,7 +31,9 @@ const Header = () => (
       <div className="bg-teal flex">
         <div className="w-2/5 h-[380px] bg-cover bg-[url('/freelance-sisallontuottaja-tablet.webp')]"></div>
         <div className="flex flex-col justify-end w-3/5 pb-12">
-          <div className="text-6xl font-bold handwriting">{nameText}</div>
+          <div className={`${caveat.className} text-6xl font-bold`}>
+            {nameText}
+          </div>
           <br />
           <div className="text-xl">
             <div>{headerText}</div>
@@ -33,7 +45,7 @@ const Header = () => (
       <div className="bg-teal flex">
         <div className="w-1/2 h-[530px] bg-cover bg-[url('/freelance-sisallontuottaja-full.webp')]"></div>
         <div className="flex flex-col justify-end w-1/2 pb-12">
-          <div className="text-8xl handwriting">{nameText}</div>
+          <div className={`${caveat.className} text-8xl`}>{nameText}</div>
           <br />
           <div className="text-2xl">
             <div>{headerText}</div>
@@ -45,7 +57,9 @@ const Header = () => (
       <div className="bg-teal flex">
         <div className="w-1/2 h-[640px] bg-cover bg-[url('/freelance-sisallontuottaja-full.webp')]"></div>
         <div className="flex flex-col justify-end w-1/2 pb-12">
-          <div className="text-4xl font-bold handwriting">{nameText}</div>
+          <div className={`${caveat.className} text-4xl font-bold`}>
+            {nameText}
+          </div>
           <br />
           <div className="text-xl">
             <div>{headerText}</div>
