653bf534c5f63288623ec4f0dd98fbb167d5db4a
Toni Leppänen <toni.leppanen@gmail.com>
2025-02-02 21:58:18 +0000

    Optimise Google font

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ config.autoAddCss = false;
 const titillium = Titillium_Web({
   weight: "400",
   subsets: ["latin"],
+  display: "swap",
 });
 
 const RootLayout = ({
@@ -16,6 +17,14 @@ const RootLayout = ({
   children: React.ReactNode;
 }>) => (
   <html lang="en">
+    <head>
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymous"
+      />
+      <link rel="dns-prefetch" href="https://fonts.gstatic.com" />
+    </head>
     <body className={titillium.className}>{children}</body>
     <GoogleAnalytics gaId="G-MCDZBZK9JH" />
   </html>
